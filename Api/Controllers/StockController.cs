617213c355025ecaf9a111bc1afd37e6c42ddb24
RuslanNnNnNn <95371964+RuslanMakhanov@users.noreply.github.com>
2024-06-07 21:29:42 +0500

    First commit

---
+++ Api/Controllers/StockController.cs
@@ -0,0 +1,34 @@
+﻿using Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public StockController(ApplicationDBContext context)
+        {
+            _dbContext = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _dbContext.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _dbContext.Stocks.Find(id);
+            if (stock == null) 
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+        
+    }
+}
---
+++ Api/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        //Navigation property
+        public Stock? Stock { get; set; }
+    }
+}
